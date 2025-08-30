269b61efb79579aa3b4377052b3695f9b91323d5
Tipu-Sultan <teepukhan729@gmail.com>
2025-08-31 01:00:06 +0530

    changes done

--- server/config/db.js
+++ server/config/db.js
@@ -2,15 +2,12 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'gaming_platform',
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-console.log('MySQL connection pool created');
+console.log('MySQL connection pool created using URI');
 
 module.exports = pool;
